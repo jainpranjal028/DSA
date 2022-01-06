b041c476ae1e2f05e904115aac718c014f3d2737
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-06 18:35:19 +0530

    Create 766. Toeplitz Matrix.java

---
+++ 766. Toeplitz Matrix.java
@@ -0,0 +1,25 @@
+//  766. Toeplitz Matrix.java
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        
+        if(matrix.length == 1 && matrix[0].length == 1)
+        {
+               return true;
+        }
+        
+        int lengthR = matrix.length - 1, lengthC = matrix[0].length - 1;
+         
+        for(int i =0 ; i<lengthR; i++)
+        {
+            for(int j =0; j<lengthC; j++)
+            {
+                if(matrix[i][j] != matrix[i+1][j+1])
+                {
+                    return false;
+                } 
+            }
+        }
+        return true;
+    }
+}
