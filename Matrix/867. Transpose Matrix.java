842eb299ce9ba591182178feb9e6dd9cf9e3292c
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-11 20:36:41 +0530

    Create 867. Transpose Matrix.java

---
+++ Matrix/867. Transpose Matrix.java
@@ -0,0 +1,21 @@
+//867. Transpose Matrix
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int M = matrix.length; int N = matrix[0].length;
+
+	//result matrix (w/ size from transposition)
+	int[][] B = new int[N][M];
+
+	//iterate thru A
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			//transpose
+			B[j][i] = matrix[i][j];
+		}
+	}
+
+	return B;
+    }
+}
