ecfaef0d5d923e6f01648567e08350264dbde685
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-14 23:51:02 +0530

    Create 1929. Concatenation of Array.java

---
+++ 1929. Concatenation of Array.java
@@ -0,0 +1,20 @@
+//1929. Concatenation of Array
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        int arr[] = new int[(nums.length * 2)];
+        
+        for(int i = 0; i < nums.length; i++){
+		
+            //first half of output array
+            arr[i] = nums[i];
+			
+            //second half of output array
+            arr[i + nums.length] = nums[i];
+        
+        }
+        
+        return arr;
+    }
+}    
